87a845e5adb1cd25a6ca92903bdc509d5aa832ab
Vorrapol Gundthanachart <v.gundthanachart@gmail.com>
2024-01-14 17:46:20 +0700

    ex02 ex05

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -6,13 +6,13 @@
 /*   By: vgundtha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 19:38:04 by vgundtha          #+#    #+#             */
-/*   Updated: 2024/01/14 09:01:12 by vgundtha         ###   ########.fr       */
+/*   Updated: 2024/01/14 14:42:39 by vgundtha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
